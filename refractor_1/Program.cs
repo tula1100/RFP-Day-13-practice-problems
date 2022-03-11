ed2a297b040ab7a875607cd6bad31e104fc56461
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-11 14:26:28 +0530

    added refrator problem

---
+++ refractor_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refractor_1
+{
+    internal class Day_13_uc_Generics
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Find Max Int  Number using Generic Method");
+            Refactor_to_generic.FindMax<int>(11, 22, 9);
+
+            Console.WriteLine("Find Max Double  Number using Generic Method");
+            Refactor_to_generic.FindMax<double>(11.3, 22.23, 9.55);
+
+            Console.WriteLine("Find Max String Number using Generic Method");
+            Refactor_to_generic.FindMax<string>("22", "33", "44");
+
+        }
+    }
+}
---
+++ refractor_1/Refactor_to_generic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refractor_1
+{
+    internal class Refactor_to_generic
+    {
+        public static T FindMax<T>(T a, T b, T c) where T : IComparable<T>
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                Console.WriteLine(a);
+            }
+
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                Console.WriteLine(b);
+            }
+
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0 || c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 || c.CompareTo(a) > 0 && c.CompareTo(b) >= 0)
+            {
+                Console.WriteLine(c);
+            }
+            return a;
+        }
+
+    }
+}
