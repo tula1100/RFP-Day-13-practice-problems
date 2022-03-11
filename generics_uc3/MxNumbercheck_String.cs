72dd9c213cd894e5661e549c74bbfc79e0fc0416
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-11 14:23:38 +0530

    added uc3

---
+++ generics_uc3/MxNumbercheck_String.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_uc3
+{
+    internal class MxNumbercheck_String
+    {
+        public static string MaxNumbermethod_String(string firstNumber, string secondNumber, string thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new Exception("string numbers firstNumber,secondNumber,thirdNumber are same");
+        }
+    }
+}
+
---
+++ generics_uc3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_uc3
+{
+    internal class Day_13_uc_Generics
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("UseCase 3 Find Max String  Number");
+
+            string stringOutput = MxNumbercheck_String.MaxNumbermethod_String("11", "22", "44");
+            Console.Write(stringOutput);
+
+        }
+    }
+}
