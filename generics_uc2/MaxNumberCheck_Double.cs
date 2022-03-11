4474476301e7e63d2c0d7c140ee02c96148a2921
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-11 14:19:22 +0530

    added uc2

---
+++ generics_uc2/MaxNumberCheck_Double.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_uc2
+{
+    internal class MaxNumberCheck_Double
+    {
+        public static double MaxNumbermethod_Double(double firstNumber, double secondNumber, double thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new Exception("double numbers firstNumber,secondNumber,thirdNumber are same");
+
+        }
+    }
+}
+  
---
+++ generics_uc2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_uc2
+{
+    internal class Day_13_uc_Generics
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("UseCase 2 Find Max Double  Number");
+
+            double doubleOutput = MaxNumberCheck_Double.MaxNumbermethod_Double(112.3, 22.23, 33.6);
+            Console.Write(doubleOutput);
+
+        }
+    }
+}
