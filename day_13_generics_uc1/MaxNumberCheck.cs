f5cc913c57a6597b97a4c717326a3b0d2666db81
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-11 14:13:14 +0530

    added uc-1

---
+++ day_13_generics_uc1/MaxNumberCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_13_generics_uc1
+{
+    internal class MaxNumberCheck
+    {
+        public static int MaxNumbermethod_Int(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new Exception("double numbers firstNumber,secondNumber,thirdNumber are same");
+
+        }
+
+    }
+}
---
+++ day_13_generics_uc1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_13_generics_uc1
+{
+    internal class Day_13_uc_Generics
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("UseCase 1 Find Max Int  Number");
+
+            int intOutput = MaxNumberCheck.MaxNumbermethod_Int(11, 22, 33);
+            Console.Write(intOutput);
+
+        }
+    }
+}
