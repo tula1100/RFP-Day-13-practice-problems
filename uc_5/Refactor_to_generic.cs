392e4adcf8bf1323c190d031e4fac40aec880776
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-11 14:36:48 +0530

    added uc-5

---
+++ uc_5/Refactor_to_generic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc_4_5
+{
+    internal class Refactor_to_generic<T>
+    {
+        public static T FindMax<T>(T a, T b, T c, T d) where T : IComparable<T>
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 && a.CompareTo(d) > 0 || a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 && a.CompareTo(d) >= 0 || a.CompareTo(b) > 0 && a.CompareTo(c) >= 0 && a.CompareTo(d) > 0)
+            {
+                Console.WriteLine(a);
+            }
+
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 && b.CompareTo(d) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 && b.CompareTo(d) >= 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0 && b.CompareTo(d) > 0)
+            {
+                Console.WriteLine(b);
+            }
+
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0 && c.CompareTo(d) > 0 || c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 && c.CompareTo(d) >= 0 || c.CompareTo(a) > 0 && c.CompareTo(b) >= 0 && c.CompareTo(d) > 0)
+            {
+                Console.WriteLine(c);
+            }
+            if (d.CompareTo(a) > 0 && d.CompareTo(b) > 0 && d.CompareTo(c) > 0 || d.CompareTo(a) >= 0 && d.CompareTo(c) > 0 && d.CompareTo(c) >= 0 || d.CompareTo(b) > 0 && d.CompareTo(c) >= 0 && d.CompareTo(c) > 0)
+            {
+                Console.WriteLine(d);
+            }
+            return a;
+
+        }
+
+    }
+}
+
